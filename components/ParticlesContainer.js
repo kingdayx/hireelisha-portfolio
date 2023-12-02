27e4712f80eea5ab0731a61035082cfd53101c67
kingdayx <elishaday989@gmail.com>
2023-12-02 13:57:02 -0500

    updated particles

--- components/ParticlesContainer.js
+++ components/ParticlesContainer.js
@@ -32,59 +32,59 @@ const ParticlesContainer = () => {
               mode: "repulse",
             },
             resize: true,
-            modes: {
-              push: {
-                quantity: 90,
-              },
-              repulse: {
-                distance: 200,
-                duration: 0.4,
-              },
+          },
+          modes: {
+            push: {
+              quantity: 90,
+            },
+            repulse: {
+              distance: 200,
+              duration: 0.4,
             },
           },
-          particles: {
-            color: {
-              value: "#e68e2e",
-            },
-            links: {
-              color: "#f5d393",
-              distance: 150,
-              enable: true,
-              opacity: 0.5,
-              width: 1,
-            },
-            collisions: {
-              enable: true,
-            },
-            move: {
-              directions: "none",
-              enable: true,
-              outModes: {
-                default: "bounce",
-              },
-              random: "false",
-              speed: 1,
-              straight: false,
-            },
-            number: {
-              density: {
-                enable: true,
-                area: 800,
-              },
-              value: 80,
-            },
-            opacity: {
-              value: 0.5,
-            },
-            shape: {
-              type: "circle",
-            },
-            size: {
-              value: { min: 1, max: 5 },
-            },
-          },
-          detectRetina: true,
         },
+        particles: {
+          color: {
+            value: "#e68e2e",
+          },
+          links: {
+            color: "#f5d393",
+            distance: 150,
+            enable: true,
+            opacity: 0.5,
+            width: 1,
+          },
+          collisions: {
+            enable: true,
+          },
+          move: {
+            directions: "none",
+            enable: true,
+            outModes: {
+              default: "bounce",
+            },
+            random: "false",
+            speed: 1,
+            straight: false,
+          },
+          number: {
+            density: {
+              enable: true,
+              area: 800,
+            },
+            value: 80,
+          },
+          opacity: {
+            value: 0.5,
+          },
+          shape: {
+            type: "circle",
+          },
+          size: {
+            value: { min: 1, max: 5 },
+          },
+        },
+        detectRetina: true,
       }}
     />
   );
