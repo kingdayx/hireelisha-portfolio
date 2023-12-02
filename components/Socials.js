7994d69ab94e5b6bf0f2488bed2ec6ea745e309c
kingdayx <elishaday989@gmail.com>
2023-12-02 14:33:11 -0500

    updated socials

--- components/Socials.js
+++ components/Socials.js
@@ -1,11 +1,22 @@
 import Link from "next/link";
-import { RiLinkedinLine } from "react-icons/ri";
+import { RiLinkedinLine, RiGithubLine } from "react-icons/ri";
 
 const Socials = () => {
   return (
     <div className="flex items-center gap-x-5 text-lg">
-      <Link href={""} className="hover:text-accent transition-all duration-300">
+      <Link
+        href={"https://www.linkedin.com/in/elisha-day-jenkins-64b1b6132/"}
+        className="hover:text-accent transition-all duration-300"
+        target="_blank"
+      >
         <RiLinkedinLine />
+      </Link>
+      <Link
+        href={"https://github.com/kingdayx"}
+        className="hover:text-accent transition-all duration-300"
+        target="_blank"
+      >
+        <RiGithubLine />
       </Link>
     </div>
   );
