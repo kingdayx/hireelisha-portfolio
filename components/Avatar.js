728b24ae65972a0d53bf0094c8a86aef8eb56ad5
kingdayx <elishaday989@gmail.com>
2023-12-02 11:28:54 -0500

    updated avatar

--- components/Avatar.js
+++ components/Avatar.js
@@ -5,7 +5,7 @@ const Avatar = () => {
     <div className="hidden xl:flex xl:max-w-none ">
       <Image
         className="translate-z-0 w-full h-full"
-        src={"/avatar.png"}
+        src={"/avatar.jpg"}
         width={737}
         height={678}
         alt="image of elisha"
