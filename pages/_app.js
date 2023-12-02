b5259b5c061e06f68ff84401d0afba988027ad19
kingdayx <elishaday989@gmail.com>
2023-12-02 11:57:08 -0500

    trying to make favicon appear

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,22 @@
 import Layout from "../components/Layout";
 import { useRouter } from "next/router";
+import Head from "next/head";
 import Transition from "../components/Transition";
 import { motion, AnimatePresence } from "framer-motion";
 import "../styles/globals.css";
+
+export const metadata = {
+  title: "Elishas Portfolio",
+  description: "Hire Elisha",
+};
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
   return (
     <Layout>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <AnimatePresence mode="wait">
         <motion.div key={router.route} className="h-full">
           <Transition />
