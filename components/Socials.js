31c9716ccf635bc1671a4d64316120cd4da0a6c5
kingdayx <elishaday989@gmail.com>
2023-12-02 14:26:35 -0500

    updated socials

--- components/Socials.js
+++ components/Socials.js
@@ -1,5 +1,14 @@
+import Link from "next/link";
+import { RiLinkedinLine } from "react-icons/ri";
+
 const Socials = () => {
-  return <div>Socials</div>;
+  return (
+    <div className="flex items-center gap-x-5 text-lg">
+      <Link href={""} className="hover:text-accent transition-all duration-300">
+        <RiLinkedinLine />
+      </Link>
+    </div>
+  );
 };
 
 export default Socials;
