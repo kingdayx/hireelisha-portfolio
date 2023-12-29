be1998f137d3ac34ddf31d5aaa6205a3ddde6e68
kingdayx <54965488+kingdayx@users.noreply.github.com>
2023-12-29 02:26:49 -0500

    update about

--- pages/about/index.js
+++ pages/about/index.js
@@ -14,6 +14,11 @@ import {
   SiAdobexd,
   SiAdobephotoshop,
 } from "react-icons/si";
+import Avatar from "../../components/Avatar";
+import Circles from "../../components/Circles";
+import { useState } from "react";
+import { motion } from "framer-motion";
+import { fadeIn } from "../../variants";
 
 //  data
 const aboutData = [
@@ -92,7 +97,54 @@ const aboutData = [
 ];
 
 const About = () => {
-  return <div>About</div>;
+  const [index, setIndex] = useState();
+  console.log(aboutData[1].info[1].title);
+  return (
+    <div className="h-full bg-primary/30 py-32 text-center xl:text-left">
+      <Circles />
+      <motion.div
+        variants={fadeIn("right", 0.2)}
+        initial="hidden"
+        animate="show"
+        exit="hidden"
+        className="hidden xl:flex absolute bottom-0 -left-[300px]"
+      >
+        <Avatar />
+      </motion.div>
+      <div className="container mx-auto h-full flex flex-col items-center xl:flex-row gap-x-6">
+        <div>text</div>
+        <div>
+          <div className="flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4">
+            {aboutData.map((item, itemIndex) => {
+              return (
+                <div
+                  key={itemIndex}
+                  className={`${
+                    index === itemIndex &&
+                    "text-accent after:w-[100%] after:bg-accent after:transition-all after:duration-300"
+                  } cursor-pointer capatilize xl:text-lg 
+            relative after:w-8 after:h-[2px]
+             after:bg-white after:absolute after:-bottom-1 after:left-0`}
+                  onClick={() => setIndex(itemIndex)}
+                >
+                  {item.title}
+                </div>
+              );
+            })}
+          </div>
+          <div>
+            {aboutData.map((item, itemIndex) => {
+              return (
+                <div key={itemIndex}>
+                  <div> {item.info[itemIndex].title} </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
